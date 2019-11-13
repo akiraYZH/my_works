6c5e3457017dc2e280bc99c35e45245498ae29cd
akira <akira@akiras-MacBook-Pro.local>
2019-11-13 09:25:33 +0800

    added getMousePos

---
+++ js_template/getMousePos/getMousePos.js
@@ -0,0 +1,6 @@
+function getMousePos(ev){
+    var scrollLeft = document.documentElement.scrollLeft || document.body.scrollLeft;
+    var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+
+    return {x:ev.clientX + scrollLeft + 'px', y:ev.clientY + scrollTop + 'px'}
+}
